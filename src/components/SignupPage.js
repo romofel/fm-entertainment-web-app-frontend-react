03b103047ef7867199b015720b3b2d007039d573
romofel <81869916+romofel@users.noreply.github.com>
2022-07-13 22:37:08 +0000

    Scaffold the Signup Component

---
+++ src/components/SignupPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+
+export default function SignupPage () {
+  return (
+    <div className="signup-page">
+      <img src={logo} alt="logo" />
+
+      <div className="signup-form">
+        <form action="">
+          <h1 className="signup-header">Sign Up</h1>
+
+          <input className="signup-email" type="email" name="email" id="email" placeholder="Email address" />
+          <input className="signup-password" type="password" name="password" id="password" placeholder="Password" />
+          <input className="signup-repassword" type="password" name="repassword" id="repassword" placeholder="Repeat password" />
+
+          <button type="submit" className="signup-submit">Create an account</button>
+
+          <p className="signup-text">Already have an account? <a href="#">Login</a></p>
+        </form>
+      </div>
+    </div>
+  );
+}
