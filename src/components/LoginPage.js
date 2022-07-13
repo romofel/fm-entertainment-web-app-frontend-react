c7474281ce3b29368e7b36d4ea6eea87839ec0cc
romofel <81869916+romofel@users.noreply.github.com>
2022-07-13 00:12:13 +0000

    Add styling classes

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,17 +6,17 @@ import logo from '../assets/logo.svg';
 export default function LoginPage() {
   return (
     <>
-      <img alt="logo" src={logo} />
+      <img className="logo" alt="logo" src={logo} />
       <div className="login-form">
         <form action="">
           <h1 className="login-header">Login</h1>
 
-          <input type="email" name="email" id="email" />
-          <input type="password" name="password" id="password" />
+          <input className="login-email" type="email" name="email" id="email" placeholder="Email address" />
+          <input className="login-password" type="password" name="password" id="password" placeholder="Password" />
 
           <button type="submit" className="login-submit">Login to your account</button>
 
-          <p className="login">Don't have an account? <a href="#">Sign Up</a></p>
+          <p className="login-text">Don't have an account? <a href="#">Sign Up</a></p>
         </form>
       </div>
     </>
