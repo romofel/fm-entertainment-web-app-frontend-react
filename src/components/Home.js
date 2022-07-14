378fc9302b833507178a97ec2a523ca282889070
romofel <81869916+romofel@users.noreply.github.com>
2022-07-14 00:31:22 +0000

    Display SearchBar Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Nav from './Nav';
+import SearchBar from './SearchBar';
 
 export default function Home() {
   return <div>
     <Nav />
+    <SearchBar />
     Home
     </div>;
 }
