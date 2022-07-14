0b156ec1f167896c422c606c4a3763debe0c9e8f
romofel <81869916+romofel@users.noreply.github.com>
2022-07-14 00:06:06 +0000

    Display all icons in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import logo from '../assets/logo.svg';
+import home from '../assets/icon-nav-home.svg';
+import movies from '../assets/icon-nav-movies.svg';
+import series from '../assets/icon-nav-tv-series.svg';
+import bookmark from '../assets/icon-nav-bookmark.svg';
 
 export default function Nav () {
   return (
-    <div>Nav Component</div>
+    <nav>
+      <img src={logo} alt="logo" />
+      <ul className="mid-nav">
+        <li><a href="#"><img src={home} alt="home nav" /></a></li>
+        <li><a href="#"><img src={movies} alt="movies nav" /></a></li>
+        <li><a href="#"><img src={series} alt="series nav" /></a></li>
+        <li><a href="#"><img src={bookmark} alt="bookmarks nav" /></a></li>
+      </ul>
+    </nav>
   );
 }
