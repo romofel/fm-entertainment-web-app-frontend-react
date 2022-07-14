7a95304bd08830e99e65ebefba044edc1a7ed5dc
romofel <81869916+romofel@users.noreply.github.com>
2022-07-14 16:24:30 +0000

    Create placeholder TrendingSection component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,16 @@ import React from 'react';
 import Nav from './Nav';
 import SearchBar from './SearchBar';
 
+function TrendingSection () {
+  return (
+    <div>TrendingSection Component</div>
+  );
+}
+
 export default function Home() {
   return <div>
     <Nav />
     <SearchBar />
-    Home
+    <TrendingSection />
     </div>;
 }
