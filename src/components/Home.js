9ff1b05f64baa7665489e02fd381c35f6cf0755f
romofel <81869916+romofel@users.noreply.github.com>
2022-07-14 17:25:56 +0000

    Import body of trending movie from html version of project

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,36 @@ import React from 'react';
 import Nav from './Nav';
 import SearchBar from './SearchBar';
 import iconBookmarkEmpty from '../assets/icon-bookmark-empty.svg';
+import iconCategoryMovie from '../assets/icon-category-movie.svg';
 
 function TrendingMovie () {
   return (
     <div className="trending-movie">
       <div className="bookmark-icon">
         <img src={iconBookmarkEmpty} alt="empty bookmark" />
+      </div>
+
+      <div className="trending-movie-details-section">
+        <ul className="trending-movie-details-section-top">
+          <li>
+            <p>2019</p>
+          </li>
+          <li>
+            <div class="separator"></div>
+          </li>
+          <li>
+            <img src={iconCategoryMovie} alt="movie icon" />
+            <p>Movie</p>
+          </li>
+          <li>
+            <div class="separator"></div>
+          </li>
+          <li>
+            <p>PG</p>
+          </li>
+        </ul>
+
+        <h3 className="trending-movie-title">Beyond Earth</h3>
       </div>
     </div>
   );
@@ -32,7 +56,7 @@ function RecommendedSection () {
 }
 
 export default function Home() {
-  return <div>
+  return <div style={{backgroundColor: 'black'}}>
     <Nav />
     <SearchBar />
     <TrendingSection />
