a46c3a91e723bdfc23862015e0248df99690a902
romofel <81869916+romofel@users.noreply.github.com>
2022-07-14 18:55:54 +0000

    Refactor TrendingMovie into its own file

---
+++ src/components/TrendingMovie.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import iconBookmarkEmpty from '../assets/icon-bookmark-empty.svg';
+import iconCategoryMovie from '../assets/icon-category-movie.svg';
+
+export default function TrendingMovie () {
+  return (
+    <div className="trending-movie">
+      <div className="bookmark-icon">
+        <img src={iconBookmarkEmpty} alt="empty bookmark" />
+      </div>
+
+      <div className="trending-movie-details-section">
+        <ul className="trending-movie-details-section-top">
+          <li>
+            <p>2019</p>
+          </li>
+          <li>
+            <div class="separator"></div>
+          </li>
+          <li>
+            <img src={iconCategoryMovie} alt="movie icon" />
+            <p>Movie</p>
+          </li>
+          <li>
+            <div class="separator"></div>
+          </li>
+          <li>
+            <p>PG</p>
+          </li>
+        </ul>
+
+        <h3 className="trending-movie-title">Beyond Earth</h3>
+      </div>
+    </div>
+  );
+}
