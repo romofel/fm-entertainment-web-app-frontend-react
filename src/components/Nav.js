104105e637cf572a0e464b5b16cfdb17a660d080
romofel <81869916+romofel@users.noreply.github.com>
2022-07-14 00:12:35 +0000

    Display avatar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,17 +4,23 @@ import home from '../assets/icon-nav-home.svg';
 import movies from '../assets/icon-nav-movies.svg';
 import series from '../assets/icon-nav-tv-series.svg';
 import bookmark from '../assets/icon-nav-bookmark.svg';
+import avatar from '../assets/image-avatar.png';
 
 export default function Nav () {
   return (
     <nav>
       <img src={logo} alt="logo" />
+      
       <ul className="mid-nav">
         <li><a href="#"><img src={home} alt="home nav" /></a></li>
         <li><a href="#"><img src={movies} alt="movies nav" /></a></li>
         <li><a href="#"><img src={series} alt="series nav" /></a></li>
         <li><a href="#"><img src={bookmark} alt="bookmarks nav" /></a></li>
       </ul>
+
+      <div className="avatar">
+        <img src={avatar} alt="avatar" />
+      </div>
     </nav>
   );
 }
