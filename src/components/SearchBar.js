66556094cf9662c5de1c0819c76ed193cfa0509d
romofel <81869916+romofel@users.noreply.github.com>
2022-07-14 16:20:05 +0000

    Scaffold SearchBar Component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import iconsearch from '../assets/icon-search.svg';
 
 export default function SearchBar () {
   return (
-    <div>SearchBar Component</div>
+    <div className="search-bar">
+      <img src={iconsearch} alt="search icon" />
+      <input type="search" name="search" id="search" placeholder="Search for movies or TV series" />
+    </div>
   );
 }
