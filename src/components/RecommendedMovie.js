0e08818ed5adfab192f750103de22758f795603f
romofel <81869916+romofel@users.noreply.github.com>
2022-07-16 19:34:13 +0000

    Create RecommendedMovie component

---
+++ src/components/RecommendedMovie.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import iconBookmarkEmpty from '../assets/icon-bookmark-empty.svg';
+import iconCategoryMovie from '../assets/icon-category-movie.svg';
+
+export default function RecommendedMovie() {
+  return (
+    <div className="recommended-movie">
+      <div className="recommended-movie-poster">
+        <div className="bookmark-icon">
+          <img src={iconBookmarkEmpty} alt="empty bookmark" />
+        </div>
+      </div>
+      <ul className="recommended-movie-details">
+        <li>
+          <p>2019</p>
+        </li>
+        <li>
+          <div className="separator"></div>
+        </li>
+        <li>
+          <img src={iconCategoryMovie} alt="movie" />
+          <p>Movie</p>
+        </li>
+        <li>
+          <div className="separator"></div>
+        </li>
+        <li>
+          <p>E</p>
+        </li>
+      </ul>
+      <h3 className="recommended-movie-title">The Great Lands</h3>
+    </div>
+  );
+}
