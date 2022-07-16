f8e6bf67f6d95d68f7df62ba4932c84922641370
romofel <81869916+romofel@users.noreply.github.com>
2022-07-16 19:26:55 +0000

    Import recommended html

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,7 +17,129 @@ function TrendingSection() {
 }
 
 function RecommendedSection() {
-  return <div>RecommendedSection Component</div>;
+  return (
+    <section id="recommended">
+      <h2 class="recommended-heading">Recommended for you</h2>
+      <div class="recommended-grid">
+        <div class="recommended-movie">
+          <div class="recommended-movie-poster">
+            <div class="bookmark-icon">
+              <img
+                src="./assets/icon-bookmark-empty.svg"
+                alt="empty bookmark"
+              />
+            </div>
+          </div>
+          <ul class="recommended-movie-details">
+            <li>
+              <p>2019</p>
+            </li>
+            <li>
+              <div class="separator"></div>
+            </li>
+            <li>
+              <img src="./assets/icon-category-movie.svg" alt="movie" />
+              <p>Movie</p>
+            </li>
+            <li>
+              <div class="separator"></div>
+            </li>
+            <li>
+              <p>E</p>
+            </li>
+          </ul>
+          <h3 class="recommended-movie-title">The Great Lands</h3>
+        </div>
+        <div class="recommended-movie">
+          <div class="recommended-movie-poster">
+            <div class="bookmark-icon">
+              <img
+                src="./assets/icon-bookmark-empty.svg"
+                alt="empty bookmark"
+              />
+            </div>
+          </div>
+          <ul class="recommended-movie-details">
+            <li>
+              <p>2019</p>
+            </li>
+            <li>
+              <div class="separator"></div>
+            </li>
+            <li>
+              <img src="./assets/icon-category-movie.svg" alt="movie" />
+              <p>Movie</p>
+            </li>
+            <li>
+              <div class="separator"></div>
+            </li>
+            <li>
+              <p>E</p>
+            </li>
+          </ul>
+          <h3 class="recommended-movie-title">The Great Lands</h3>
+        </div>
+        <div class="recommended-movie">
+          <div class="recommended-movie-poster">
+            <div class="bookmark-icon">
+              <img
+                src="./assets/icon-bookmark-empty.svg"
+                alt="empty bookmark"
+              />
+            </div>
+          </div>
+          <ul class="recommended-movie-details">
+            <li>
+              <p>2019</p>
+            </li>
+            <li>
+              <div class="separator"></div>
+            </li>
+            <li>
+              <img src="./assets/icon-category-movie.svg" alt="movie" />
+              <p>Movie</p>
+            </li>
+            <li>
+              <div class="separator"></div>
+            </li>
+            <li>
+              <p>E</p>
+            </li>
+          </ul>
+          <h3 class="recommended-movie-title">The Great Lands</h3>
+        </div>
+        <div class="recommended-movie">
+          <div class="recommended-movie-poster">
+            <div class="bookmark-icon">
+              <img
+                src="./assets/icon-bookmark-empty.svg"
+                alt="empty bookmark"
+              />
+            </div>
+          </div>
+          <ul class="recommended-movie-details">
+            <li>
+              <p>2019</p>
+            </li>
+            <li>
+              <div class="separator"></div>
+            </li>
+            <li>
+              <img src="./assets/icon-category-movie.svg" alt="movie" />
+              <p>Movie</p>
+            </li>
+            <li>
+              <div class="separator"></div>
+            </li>
+            <li>
+              <p>E</p>
+            </li>
+          </ul>
+          <h3 class="recommended-movie-title">The Great Lands</h3>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default function Home() {
