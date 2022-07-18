e190cca30a5e556ca15d35e1f717f030a5d90051
romofel <81869916+romofel@users.noreply.github.com>
2022-07-18 11:36:04 +0000

    Formatting

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,16 @@ import React from 'react';
 import iconsearch from '../assets/icon-search.svg';
 import './SearchBar.css';
 
-export default function SearchBar () {
+export default function SearchBar() {
   return (
     <div className="search-bar">
       <img src={iconsearch} alt="search icon" />
-      <input type="search" name="search" id="search" placeholder="Search for movies or TV series" />
+      <input
+        type="search"
+        name="search"
+        id="search"
+        placeholder="Search for movies or TV series"
+      />
     </div>
   );
 }
