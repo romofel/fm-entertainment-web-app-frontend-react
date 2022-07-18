0b5ab9a7efc3773a0c5ec3b68abf9f989375013f
romofel <81869916+romofel@users.noreply.github.com>
2022-07-18 11:00:16 +0000

    Add local state for bookmark and onclick to switch state

--- src/components/TrendingMovie.js
+++ src/components/TrendingMovie.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import iconBookmarkEmpty from '../assets/icon-bookmark-empty.svg';
+import iconBookmarkFull from '../assets/icon-bookmark-full.svg';
 import iconCategoryMovie from '../assets/icon-category-movie.svg';
 import './TrendingMovie.css';
 
 export default function TrendingMovie({ movie }) {
+  const [isBookmarked, setIsBookmarked] = React.useState(movie.isBookmarked);
+
   return (
     <div className="trending-movie">
-      <div className="bookmark-icon">
-        <img src={iconBookmarkEmpty} alt="empty bookmark" />
+      <div
+        className="bookmark-icon"
+        onClick={() => setIsBookmarked(!isBookmarked)}
+      >
+        <img
+          src={isBookmarked ? iconBookmarkFull : iconBookmarkEmpty}
+          alt="empty bookmark"
+        />
       </div>
 
       <div className="trending-movie-details-section">
