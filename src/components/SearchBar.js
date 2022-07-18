d966769c11ef28f1ecd095ef89b8d654a4bf3d70
romofel <81869916+romofel@users.noreply.github.com>
2022-07-18 11:52:55 +0000

    Accept onSearch prop function and call it onChange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import iconsearch from '../assets/icon-search.svg';
 import './SearchBar.css';
 
-export default function SearchBar() {
+export default function SearchBar({ onSearch }) {
   return (
     <div className="search-bar">
       <img src={iconsearch} alt="search icon" />
@@ -11,6 +11,10 @@ export default function SearchBar() {
         name="search"
         id="search"
         placeholder="Search for movies or TV series"
+        onChange={(event) => {
+          event.preventDefault();
+          onSearch(event.target.value);
+        }}
       />
     </div>
   );
