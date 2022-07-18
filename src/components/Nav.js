3821611a74075c3d4fabc0610930885196d9602a
romofel <81869916+romofel@users.noreply.github.com>
2022-07-18 11:27:25 +0000

    accept and active prop and use it to display active

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,16 +7,48 @@ import bookmark from '../assets/icon-nav-bookmark.svg';
 import avatar from '../assets/image-avatar.png';
 import './Nav.css';
 
-export default function Nav () {
+export default function Nav({ active }) {
   return (
     <nav>
       <img src={logo} alt="logo" />
 
       <ul className="mid-nav">
-        <li><a href="#"><img src={home} alt="home nav" /></a></li>
-        <li><a href="#"><img src={movies} alt="movies nav" /></a></li>
-        <li><a href="#"><img src={series} alt="series nav" /></a></li>
-        <li><a href="#"><img src={bookmark} alt="bookmarks nav" /></a></li>
+        <li>
+          <a href="#">
+            <img
+              className={active === 'home' && 'active'}
+              src={home}
+              alt="home nav"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img
+              className={active === 'movies' && 'active'}
+              src={movies}
+              alt="movies nav"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img
+              className={active === 'series' && 'active'}
+              src={series}
+              alt="series nav"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img
+              className={active === 'bookmarks' && 'active'}
+              src={bookmark}
+              alt="bookmarks nav"
+            />
+          </a>
+        </li>
       </ul>
 
       <div className="avatar">
