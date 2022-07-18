69ed378a469f4a7870af6b7596e477ee319b5937
romofel <81869916+romofel@users.noreply.github.com>
2022-07-18 08:34:30 +0000

    Split posters into regular and trending

--- src/Posters.js
+++ src/Posters.js
@@ -1,10 +1,18 @@
 import data from './data.json';
 
-const posters = new Map(
-  data.map((movie) => [
-    movie.title,
-    require(`${movie.thumbnail.regular.small}`),
-  ])
-);
+const posters = {
+  regular: new Map(
+    data.map((movie) => [
+      movie.title,
+      require(`${movie.thumbnail.regular.small}`),
+    ])
+  ),
+  trending: new Map(
+    data.map((movie) => [
+      movie.title,
+      require(`${movie.thumbnail.trending.small}`),
+    ])
+  ),
+};
 
 export default posters;
