e53f39112f7964fd3720177cc7ea2d2cc700ef01
romofel <81869916+romofel@users.noreply.github.com>
2022-07-20 16:00:42 +0000

    Add Search UI to the screen component

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,12 +1,38 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
+import data from '../data.json';
 
 import Nav from './Nav';
+import SearchBar from './SearchBar';
+import RecommendedMovie from './RecommendedMovie';
+
+function SearchSection({ search, data }) {
+  const matches = data.filter((movie) =>
+    movie.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <section id="search">
+      <h2 className="search-heading">
+        Found {matches.length} Results for '{search}'
+      </h2>
+      <div className="search-grid">
+        {matches.map((movie) => (
+          <RecommendedMovie movie={movie} />
+        ))}
+      </div>
+    </section>
+  );
+}
 
 export default function Screen() {
+  const [search, setSearch] = React.useState(null);
   return (
     <>
       <Nav active={'home'} />
-      <Outlet />
+      <SearchBar onSearch={setSearch} />
+      
+      {search ? <SearchSection search={search} data={data} /> : <Outlet />}
     </>
   );
 }
