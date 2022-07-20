042424e2facfd03d4787cb9862a88feec7526df1
romofel <81869916+romofel@users.noreply.github.com>
2022-07-20 15:25:10 +0000

    Nest routes and set Home component as the element for the index path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
+import Screen from './components/Screen';
 import Home from './components/Home';
 import Login from './components/LoginPage';
 import Signup from './components/SignupPage';
@@ -9,7 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Screen />}>
+          <Route index element={<Home />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="signup" element={<Signup />} />
       </Routes>
