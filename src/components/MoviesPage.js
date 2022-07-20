6ec1cefbf3fffec6f4775ae392938384d5190712
romofel <81869916+romofel@users.noreply.github.com>
2022-07-20 16:36:33 +0000

    Display the UI elements reusing styling classes from recommended

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,3 +1,18 @@
+import data from '../data';
+import RecommendedMovie from './RecommendedMovie';
+
 export default function MoviesPage() {
-  return <div>Movies Page</div>;
+  return (
+    <section id="recommended">
+      <h2 className="trending-heading">Movies</h2>
+
+      <div className="recommended-grid">
+        {data
+          .filter((movie) => movie.category === 'Movie')
+          .map((movie) => (
+            <RecommendedMovie key={movie.title} movie={movie} />
+          ))}
+      </div>
+    </section>
+  );
 }
