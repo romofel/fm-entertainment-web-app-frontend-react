7710609ed3ecf7981075fecb96711fae12531183
romofel <81869916+romofel@users.noreply.github.com>
2022-07-20 15:24:50 +0000

    Create Screen component

---
+++ src/components/Screen.js
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+import Nav from './Nav';
+
+export default function Screen() {
+  return (
+    <>
+      <Nav active={'home'} />
+      <Outlet />
+    </>
+  );
+}
