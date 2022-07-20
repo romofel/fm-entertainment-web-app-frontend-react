ec1e617e1cad3fa4b7d58a707e2b02e72c4e368e
romofel <81869916+romofel@users.noreply.github.com>
2022-07-20 17:18:19 +0000

    Build the component ui to display passed in data

--- src/components/ShowsSection.js
+++ src/components/ShowsSection.js
@@ -1,3 +1,13 @@
-export default function ShowsSection() {
-  return <div>ShowsSection component</div>
+import RecommendedMovie from './RecommendedMovie';
+import './ShowsSection.css';
+
+export default function ShowsSection({ section, heading, data }) {
+  return (
+    <section id={section}>
+      <h2 className="shows-heading">{heading}</h2>
+      {data.map((show) => (
+        <RecommendedMovie key={show.title} movie={show} />
+      ))}
+    </section>
+  );
 }
