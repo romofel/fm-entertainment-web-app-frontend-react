691d9c9ba7d392f6ed6db7a2bf9dd449aef95968
romofel <81869916+romofel@users.noreply.github.com>
2022-07-20 17:27:30 +0000

    Refactor MoviesPage to use ShowsSection component

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,18 +1,12 @@
 import data from '../data';
-import RecommendedMovie from './RecommendedMovie';
+import ShowsSection from './ShowsSection';
 
 export default function MoviesPage() {
   return (
-    <section id="recommended">
-      <h2 className="trending-heading">Movies</h2>
-
-      <div className="recommended-grid">
-        {data
-          .filter((movie) => movie.category === 'Movie')
-          .map((movie) => (
-            <RecommendedMovie key={movie.title} movie={movie} />
-          ))}
-      </div>
-    </section>
+    <ShowsSection
+      section="movies"
+      heading="Movies"
+      data={data.filter((movie) => movie.category === 'Movie')}
+    />
   );
 }
