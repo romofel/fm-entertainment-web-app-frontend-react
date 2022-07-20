2c8c7705a763f4eb93fa7400dd2a20e2730c9fbe
romofel <81869916+romofel@users.noreply.github.com>
2022-07-20 16:55:17 +0000

    Set placeholder text based on the location

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
 import iconsearch from '../assets/icon-search.svg';
 import './SearchBar.css';
 
+const decidePlaceholder = (path) => {
+  switch (path) {
+    case '/movies':
+      return 'Search for movies';
+    case '/series':
+      return 'Search for TV series';
+    case '/bookmarks':
+      return 'Search for bookmarked shows';
+    default:
+      return 'Search for movies or TV Series';
+  }
+};
+
 export default function SearchBar({ onSearch }) {
+  const { pathname: path } = useLocation();
+
   return (
     <div className="search-bar">
       <img src={iconsearch} alt="search icon" />
@@ -10,7 +27,7 @@ export default function SearchBar({ onSearch }) {
         type="search"
         name="search"
         id="search"
-        placeholder="Search for movies or TV series"
+        placeholder={decidePlaceholder(path)}
         onChange={(event) => {
           event.preventDefault();
           onSearch(event.target.value);
