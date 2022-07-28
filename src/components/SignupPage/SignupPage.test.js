1959e8bd08981a6381d93379ac26a794fc6e152e
romofel <81869916+romofel@users.noreply.github.com>
2022-07-28 16:39:09 +0000

    add test todos

---
+++ src/components/SignupPage/SignupPage.test.js
@@ -0,0 +1,5 @@
+it.todo('should render component without error');
+it.todo('should be able to redirect to login component');
+it.todo('should call signup api');
+it.todo('should handle a successful signup');
+it.todo('should handle a unsucsseful signup');
