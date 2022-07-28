0095419bba8eb9c0d06825f54a1b8b2c24ca353d
romofel <81869916+romofel@users.noreply.github.com>
2022-07-28 17:39:54 +0000

    Add login redirect test

--- src/components/SignupPage/SignupPage.test.js
+++ src/components/SignupPage/SignupPage.test.js
@@ -1,13 +1,33 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter, MemoryRouter, Routes, Route } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import SignupPage from './';
+import LoginPage from '../LoginPage';
 
 it('should render component without error', () => {
-  render(<SignupPage />);
+  render(<SignupPage />, { wrapper: BrowserRouter });
   const signupPage = screen.queryByTestId('signup-page');
 
   expect(signupPage).toBeInTheDocument();
 });
-it.todo('should be able to redirect to login component');
+
+it('should be able to redirect to login component', async () => {
+  render(
+    <MemoryRouter initialEntries={['/signup']}>
+      <Routes>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignupPage />} />
+      </Routes>
+    </MemoryRouter>
+  );
+  const loginButton = screen.getByTestId('login-redirect');
+
+  expect(loginButton).toBeInTheDocument();
+  userEvent.click(loginButton);
+
+  const loginPage = await screen.findByTestId('login-page');
+  expect(loginPage).toBeInTheDocument();
+});
 it.todo('should call signup api');
 it.todo('should handle a successful signup');
 it.todo('should handle a unsucsseful signup');
