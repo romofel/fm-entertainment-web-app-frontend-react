17eafc865fbc512102e274d1b1b75e102ef0a64d
romofel <81869916+romofel@users.noreply.github.com>
2022-07-28 16:44:22 +0000

    Add render component test

--- src/components/SignupPage/SignupPage.test.js
+++ src/components/SignupPage/SignupPage.test.js
@@ -1,4 +1,12 @@
-it.todo('should render component without error');
+import { render, screen } from '@testing-library/react';
+import SignupPage from './';
+
+it('should render component without error', () => {
+  render(<SignupPage />);
+  const signupPage = screen.queryByTestId('signup-page');
+
+  expect(signupPage).toBeInTheDocument();
+});
 it.todo('should be able to redirect to login component');
 it.todo('should call signup api');
 it.todo('should handle a successful signup');
