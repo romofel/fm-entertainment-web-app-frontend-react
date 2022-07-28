f4bf2bf91579637e3dbddbaedf4455f0c9a4f212
romofel <81869916+romofel@users.noreply.github.com>
2022-07-28 20:10:08 +0000

    test that on register click the api gets called

--- src/components/SignupPage/SignupPage.test.js
+++ src/components/SignupPage/SignupPage.test.js
@@ -3,6 +3,7 @@ import { BrowserRouter, MemoryRouter, Routes, Route } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import SignupPage from './';
 import LoginPage from '../LoginPage';
+import registerService from '../../services/registerService';
 
 it('should render component without error', () => {
   render(<SignupPage />, { wrapper: BrowserRouter });
@@ -28,6 +29,21 @@ it('should be able to redirect to login component', async () => {
   const loginPage = await screen.findByTestId('login-page');
   expect(loginPage).toBeInTheDocument();
 });
-it.todo('should call signup api');
+
+it('should call signup api', () => {
+  jest.spyOn(registerService, 'registerUser').mockResolvedValue({
+    registration: "SUCCESS",
+    message: "",
+  });
+  render(<SignupPage />, { wrapper: BrowserRouter });
+  const signupButton = screen.queryByTestId('signup-button');
+  
+  expect(signupButton).toBeInTheDocument();
+
+  userEvent.click(signupButton);
+
+  expect(registerService.registerUser).toHaveBeenCalledTimes(1);
+});
+
 it.todo('should handle a successful signup');
 it.todo('should handle a unsucsseful signup');
