6e49695202e14450bc285dcb81ab6b34ca9a336a
romofel <81869916+romofel@users.noreply.github.com>
2022-07-29 03:51:58 +0000

    update description for performing password match check

--- src/components/SignupPage/SignupPage.test.js
+++ src/components/SignupPage/SignupPage.test.js
@@ -77,7 +77,8 @@ it('should accept password confirmation input', () => {
   expect(passwordInput).toHaveValue('password');
 });
 
-it.todo('should check that both passwords match');
+it.todo('should perform check that both passwords match');
+
 
 it.todo('should handle a successful signup');
 it.todo('should handle a unsucsseful signup');
