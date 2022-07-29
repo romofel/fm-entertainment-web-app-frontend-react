fc167a45f22a6553e56f7698a6339ede7238aa18
romofel <81869916+romofel@users.noreply.github.com>
2022-07-29 03:34:29 +0000

    Implement local state for all the input fields

--- src/components/SignupPage/SignupPage.js
+++ src/components/SignupPage/SignupPage.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import './SignupPage.css';
@@ -6,6 +6,9 @@ import registerService from '../../services/registerService';
 
 export default function SignupPage() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repassword, setRePassword] = useState('');
 
   const handleRegister = async (event) => {
     event.preventDefault();
@@ -22,25 +25,43 @@ export default function SignupPage() {
           <h1 className="signup-header">Sign Up</h1>
 
           <input
+            data-testid="signup-email-input"
             className="signup-email"
             type="email"
             name="email"
             id="email"
             placeholder="Email address"
+            onChange={(event) => {
+              event.preventDefault();
+              setEmail(event.target.value);
+            }}
+            value={email}
           />
           <input
+            data-testid="signup-password-input"
             className="signup-password"
             type="password"
             name="password"
             id="password"
             placeholder="Password"
+            onChange={(event) => {
+              event.preventDefault();
+              setPassword(event.target.value);
+            }}
+            value={password}
           />
           <input
+            data-testid="signup-repassword-input"
             className="signup-repassword"
             type="password"
             name="repassword"
             id="repassword"
             placeholder="Repeat password"
+            onChange={(event) => {
+              event.preventDefault();
+              setRePassword(event.target.value);
+            }}
+            value={repassword}
           />
 
           <button
