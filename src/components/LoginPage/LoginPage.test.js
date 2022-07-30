430d49951f1e1262cfef13804133b2c4d5676988
romofel <81869916+romofel@users.noreply.github.com>
2022-07-30 12:26:53 +0000

    Test that email handles receiving input

--- src/components/LoginPage/LoginPage.test.js
+++ src/components/LoginPage/LoginPage.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import LoginPage from './';
 
 it('should render component without error', () => {
@@ -7,7 +8,17 @@ it('should render component without error', () => {
   const loginPage = screen.queryByTestId('login-page');
   expect(loginPage).toBeInTheDocument();
 });
-it.todo('should accept email input');
+
+it('should accept email input', () => {
+  const userEmail = 'user@email.com';
+  render(<LoginPage />);
+
+  const emailInput = screen.queryByTestId('login-email-input');
+  expect(emailInput).toBeInTheDocument();
+
+  userEvent.type(emailInput, userEmail);
+  expect(emailInput).toHaveValue(userEmail);
+});
 it.todo('should accept password input');
 it.todo('should disable login button by default');
 it.todo('should enable login button when email and password have been entered');
