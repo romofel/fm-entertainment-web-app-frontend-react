6418b593e220d5113089801c0dd1dd0e913977b5
romofel <81869916+romofel@users.noreply.github.com>
2022-07-30 10:43:41 +0000

    Add test todos

---
+++ src/components/LoginPage/LoginPage.test.js
@@ -0,0 +1,8 @@
+it.todo('should render component without error');
+it.todo('should accept email input');
+it.todo('should accept password input');
+it.todo('should disable login button by default');
+it.todo('should enable login button when email and password have been entered');
+it.todo('should redirect when Sign up is clicked');
+it.todo('should redirect on successful login');
+it.todo('should not redirect on failed login');
