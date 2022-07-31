1d2487eb755e4b3986431ae4f585314500e9aa10
romofel <81869916+romofel@users.noreply.github.com>
2022-07-31 08:08:57 +0000

    Implement Button Component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,26 +2,38 @@
 
 import { css } from '@emotion/react';
 
-export default function Button() {
+const defaultStyles = css(css`
+  display: block;
+  background-color: #fc4747;
+  color: white;
+  font-family: 'Outfit';
+  font-weight: 300;
+  font-size: 15px;
+  padding: 1rem 6rem;
+  border-radius: 0.375rem;
+  border: none;
+  &:hover {
+    color: #161d2f;
+    background-color: white;
+  }
+`);
+
+const disabledStyles = css(css`
+  background-color: grey;
+  &:hover {
+    background-color: grey;
+    color: white;
+  }
+`);
+
+export default function Button({ text, isDisabled }) {
   return (
     <button
-      css={css`
-        display: block;
-        background-color: #fc4747;
-        color: white;
-        font-family: 'Outfit';
-        font-weight: 300;
-        font-size: 15px;
-        padding: 1rem 6rem;
-        border-radius: 0.375rem;
-        border: none;
-        &:hover {
-          color: #161d2f;
-          background-color: white;
-        }
-      `}
+      onClick={() => console.log('test')}
+      disabled={isDisabled}
+      css={isDisabled ? [defaultStyles, disabledStyles] : defaultStyles}
     >
-      Button Component
+      {text}
     </button>
   );
 }
