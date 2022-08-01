aa6b7ff7dfcb18e0a5f6a46087ba14ad0152b730
romofel <81869916+romofel@users.noreply.github.com>
2022-08-01 14:53:44 +0000

    Pass validate functions and errorText to stories

--- src/components/InputField/InputField.stories.js
+++ src/components/InputField/InputField.stories.js
@@ -5,12 +5,29 @@ export default {
   component: InputField,
 };
 
-export const Email = () => <InputField placeholder="Email" type="email" />;
+export const Email = () => (
+  <InputField
+    placeholder="Email"
+    type="email"
+    validate={(text) => text.length !== 0}
+    errorText="Can't be empty"
+  />
+);
 
 export const Password = () => (
-  <InputField placeholder="Password" type="password" />
+  <InputField
+    placeholder="Password"
+    type="password"
+    validate={(text) => text.length !== 0}
+    errorText="Can't be empty"
+  />
 );
 
 export const Error = () => (
-  <InputField placeholder="Error" type="text" validate={(text) => false} errorText="Can't be empty" />
+  <InputField
+    placeholder="Error"
+    type="text"
+    validate={(text) => false}
+    errorText="Can't be empty"
+  />
 );
