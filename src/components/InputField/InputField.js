150877b4ba9dfb346eaaf4e02513f721cd226a6a
romofel <81869916+romofel@users.noreply.github.com>
2022-08-01 12:06:26 +0000

    Add error field and accept a validate prop and set a default function value to it

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -3,35 +3,42 @@
 import { css } from '@emotion/react';
 import { useState } from 'react';
 
-export default function InputField({ placeholder, type="text" }) {
+export default function InputField({
+  placeholder,
+  type = 'text',
+  validate = (_) => true,
+}) {
   const [text, setText] = useState('');
 
   return (
-    <input
-      data-testid="input-field"
-      placeholder={placeholder}
-      type={type}
-      css={css`
-        background-color: #161d2f;
-        color: rgb(255, 255, 255);
-        border: none;
-        border-bottom: 1px solid #5a698f;
-        font-family: 'Outfit';
-        font-weight: 300;
-        font-size: 15px;
-        padding: 1rem;
-        caret-color: #fc4747;
-        &::placeholder {
-          color: rgba(255, 255, 255, 0.5);
-        }
-        &:focus {
-          outline: none;
+    <div>
+      <input
+        data-testid="input-field"
+        placeholder={placeholder}
+        type={type}
+        css={css`
+          background-color: #161d2f;
+          color: rgb(255, 255, 255);
           border: none;
-          border-bottom: 1px solid white;
-        }
-      `}
-      onChange={(event) => setText(event.target.value)}
-      value={text}
-    />
+          border-bottom: 1px solid #5a698f;
+          font-family: 'Outfit';
+          font-weight: 300;
+          font-size: 15px;
+          padding: 1rem;
+          caret-color: #fc4747;
+          &::placeholder {
+            color: rgba(255, 255, 255, 0.5);
+          }
+          &:focus {
+            outline: none;
+            border: none;
+            border-bottom: 1px solid white;
+          }
+        `}
+        onChange={(event) => setText(event.target.value)}
+        value={text}
+      />
+      {validate(text) ? undefined : <p data-testid="error-field">Error</p>}
+    </div>
   );
 }
