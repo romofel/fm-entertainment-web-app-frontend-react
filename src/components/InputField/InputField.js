6dab5e7bed8a0efe8b499167fc4d3cbdfd4c4246
romofel <81869916+romofel@users.noreply.github.com>
2022-08-01 12:10:52 +0000

    Refactor css to its own variables

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -3,6 +3,30 @@
 import { css } from '@emotion/react';
 import { useState } from 'react';
 
+const containerStyles = css(css`
+  display: flex;
+`);
+
+const inputStyles = css(css`
+  background-color: #161d2f;
+  color: rgb(255, 255, 255);
+  border: none;
+  border-bottom: 1px solid #5a698f;
+  font-family: 'Outfit';
+  font-weight: 300;
+  font-size: 15px;
+  padding: 1rem;
+  caret-color: #fc4747;
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.5);
+  }
+  &:focus {
+    outline: none;
+    border: none;
+    border-bottom: 1px solid white;
+  }
+`);
+
 export default function InputField({
   placeholder,
   type = 'text',
@@ -11,30 +35,12 @@ export default function InputField({
   const [text, setText] = useState('');
 
   return (
-    <div>
+    <div css={containerStyles}>
       <input
         data-testid="input-field"
         placeholder={placeholder}
         type={type}
-        css={css`
-          background-color: #161d2f;
-          color: rgb(255, 255, 255);
-          border: none;
-          border-bottom: 1px solid #5a698f;
-          font-family: 'Outfit';
-          font-weight: 300;
-          font-size: 15px;
-          padding: 1rem;
-          caret-color: #fc4747;
-          &::placeholder {
-            color: rgba(255, 255, 255, 0.5);
-          }
-          &:focus {
-            outline: none;
-            border: none;
-            border-bottom: 1px solid white;
-          }
-        `}
+        css={inputStyles}
         onChange={(event) => setText(event.target.value)}
         value={text}
       />
