3fa13e149c272c465673c16697b0611534da56c4
romofel <81869916+romofel@users.noreply.github.com>
2022-08-01 12:06:03 +0000

    Formatting and pass validate function

--- src/components/InputField/InputField.stories.js
+++ src/components/InputField/InputField.stories.js
@@ -5,8 +5,12 @@ export default {
   component: InputField,
 };
 
-export const Email = () => <InputField placeholder="Email" type="email"/>;
+export const Email = () => <InputField placeholder="Email" type="email" />;
 
-export const Password = () => <InputField placeholder="Password" type="password" />;
+export const Password = () => (
+  <InputField placeholder="Password" type="password" />
+);
 
-export const Error = () => <InputField placeholder="Error" type="text" />;
+export const Error = () => (
+  <InputField placeholder="Error" type="text" validate={(text) => false} />
+);
