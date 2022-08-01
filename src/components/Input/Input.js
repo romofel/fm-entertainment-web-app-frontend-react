6657e799b9ba2f338631b6e5079a582840ce60ed
romofel <81869916+romofel@users.noreply.github.com>
2022-08-01 08:24:12 +0000

    Use local state and data testid to pass tests

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,14 +1,20 @@
 /** @jsxImportSource @emotion/react */
 
 import { css } from '@emotion/react';
+import { useState } from 'react';
 
 export default function Input({ placeholder }) {
+  const [text, setText] = useState('');
+
   return (
     <input
+      data-testid="input-field"
       placeholder={placeholder}
       css={css`
         background-color: #161d2f;
+        color: rgb(255, 255, 255);
         border: none;
+        border-bottom: 1px solid #5a698f;
         font-family: 'Outfit';
         font-weight: 300;
         font-size: 15px;
@@ -16,7 +22,14 @@ export default function Input({ placeholder }) {
         &::placeholder {
           color: rgba(255, 255, 255, 0.5);
         }
+        &:focus {
+          outline: none;
+          border: none;
+          border-bottom: 1px solid white;
+        }
       `}
+      onChange={(event) => setText(event.target.value)}
+      value={text}
     />
   );
 }
