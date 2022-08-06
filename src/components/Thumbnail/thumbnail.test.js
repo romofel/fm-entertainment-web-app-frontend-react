1856b09882097c2c2dad38866e3a51919d6660ee
romofel <81869916+romofel@users.noreply.github.com>
2022-08-06 17:04:24 +0000

    Test that the component renders a background by default

--- src/components/Thumbnail/thumbnail.test.js
+++ src/components/Thumbnail/thumbnail.test.js
@@ -1,1 +1,9 @@
-test.todo('that component renders properly');
+import { render, screen } from '@testing-library/react';
+import Thumbnail from './';
+
+it('should render only the background by default', () => {
+  render(<Thumbnail />);
+  const thumbnail = screen.queryByTestId('thumbnail-background');
+
+  expect(thumbnail).toBeInTheDocument();
+});
