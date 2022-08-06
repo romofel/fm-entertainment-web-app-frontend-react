7c2575565f47a18524d8dfb825be7be7fd8aaea4
romofel <81869916+romofel@users.noreply.github.com>
2022-08-06 17:22:43 +0000

    Test background prop

--- src/components/Thumbnail/thumbnail.test.js
+++ src/components/Thumbnail/thumbnail.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Thumbnail from './';
+import trending from '../../assets/thumbnails/1998/trending/large.jpg';
 
 it('should render only the background by default', () => {
   render(<Thumbnail />);
+  const thumbnail = screen.queryByTestId('thumbnail-background');
+
+  expect(thumbnail).toBeInTheDocument();
+});
+
+it('should render background when one is passed through props', () => {
+  render(<Thumbnail background={trending} />);
   const thumbnail = screen.queryByTestId('thumbnail-background');
 
   expect(thumbnail).toBeInTheDocument();
