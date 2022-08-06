56caca88a5f5c14c809b44826d87bd9dd38e996c
romofel <81869916+romofel@users.noreply.github.com>
2022-08-06 17:20:02 +0000

    Set default background and display it when one is not passed through props

--- src/components/Thumbnail/thumbnail.jsx
+++ src/components/Thumbnail/thumbnail.jsx
@@ -1,7 +1,8 @@
 import trending from '../../assets/thumbnails/1998/trending/large.jpg';
 
-export default function Thumbnail() {
+export default function Thumbnail({ background = 'https://placehold.co/600x400' }) {
   return <div>
-    <img data-testid="thumbnail-background" src={trending} alt="" />
+    {background && <img data-testid="thumbnail-background" src={background} alt="" />
+    }
   </div>;
 }
