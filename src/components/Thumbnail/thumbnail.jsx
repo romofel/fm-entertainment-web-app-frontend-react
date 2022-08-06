93d45d028128bc1512f344a1d05573d1109c9d71
romofel <81869916+romofel@users.noreply.github.com>
2022-08-06 17:07:19 +0000

    Display a background

--- src/components/Thumbnail/thumbnail.jsx
+++ src/components/Thumbnail/thumbnail.jsx
@@ -1,3 +1,7 @@
+import trending from '../../assets/thumbnails/1998/trending/large.jpg';
+
 export default function Thumbnail() {
-  return <div>Thumbnail Component</div>;
+  return <div>
+    <img data-testid="thumbnail-background" src={trending} alt="" />
+  </div>;
 }
