d78eeb2813a2fb359d4c6105a2910c474160945a
romofel <81869916+romofel@users.noreply.github.com>
2022-08-06 14:52:20 +0000

    Add styles to the component

--- src/components/BookmarkIcon/bookmarkicon.jsx
+++ src/components/BookmarkIcon/bookmarkicon.jsx
@@ -1,3 +1,22 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
+import bookmarkFull from '../../assets/icon-bookmark-full.svg';
+import bookmarkEmpty from '../../assets/icon-bookmark-empty.svg';
+
+const componentStyles = ({ backgroundColor='rgba(16 20 30 / 50%)' }) => css`
+  background-color: ${backgroundColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-self: flex-end;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 5rem;
+`;
+
 export default function BookmarkIcon() {
-  return <div>BookmarkIcon Component</div>
+  return <div css={componentStyles({})}>
+    <img src={bookmarkEmpty} alt="bookmark svg" />
+  </div>
 }
