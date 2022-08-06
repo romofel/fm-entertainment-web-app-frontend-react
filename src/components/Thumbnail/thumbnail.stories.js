b15c51fc870f144a680c90328e1d4aaadd380bc4
romofel <81869916+romofel@users.noreply.github.com>
2022-08-06 17:20:23 +0000

    Create empty story and pass background prop to idle story

--- src/components/Thumbnail/thumbnail.stories.js
+++ src/components/Thumbnail/thumbnail.stories.js
@@ -1,8 +1,10 @@
 import Thumbnail from './';
+import trending from '../../assets/thumbnails/1998/trending/large.jpg';
 
 export default {
   title: 'Thumbnail',
   component: Thumbnail,
 };
 
-export const Idle = () => <Thumbnail />;
+export const Empty = () => <Thumbnail />;
+export const Idle = () => <Thumbnail background={trending} />;
