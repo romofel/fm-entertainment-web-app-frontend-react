2ed4bee2d0078f559787029ec4709cfe6a88dfca
Felipe Romo <felipe@romofel.dev>
2022-08-13 01:20:14 -0500

    Format

--- src/components/Thumbnail/thumbnail.jsx
+++ src/components/Thumbnail/thumbnail.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const defaultBackground = 'https://placehold.co/600x400';
+const defaultBackground = "https://placehold.co/600x400";
 
 function PlayContainer() {
   return <div data-testid="play-container">PlayContainer</div>;
@@ -9,17 +9,20 @@ function PlayContainer() {
 export default function Thumbnail({ background = defaultBackground }) {
   const [onHover, setOnHover] = useState(false);
 
-  return <div onMouseEnter={() => {
-    setOnHover(true);
-  }}
-    onMouseLeave={() => {
-      setOnHover(false);
-    }}
-  >
-    {
-      onHover && <PlayContainer />
-    }
-    {background && <img data-testid="thumbnail-background" src={background} alt="" />
-    }
-  </div>;
+  return (
+    <div
+      onMouseEnter={() => {
+        setOnHover(true);
+      }}
+      onMouseLeave={() => {
+        setOnHover(false);
+      }}
+    >
+      {onHover && <PlayContainer />}
+      {background && (
+        <img data-testid="thumbnail-background" src={background} alt="" />
+      )}
+    </div>
+  );
 }
+
