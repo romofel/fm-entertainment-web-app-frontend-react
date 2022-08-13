a17cc253ef7d59a3510073881bd2bdc9c765abea
romofel <81869916+romofel@users.noreply.github.com>
2022-08-13 05:49:28 +0000

    update thumbnail component

--- src/components/Thumbnail/thumbnail.jsx
+++ src/components/Thumbnail/thumbnail.jsx
@@ -1,7 +1,24 @@
-import trending from '../../assets/thumbnails/1998/trending/large.jpg';
+import { useState } from 'react';
 
-export default function Thumbnail({ background = 'https://placehold.co/600x400' }) {
-  return <div>
+const defaultBackground = 'https://placehold.co/600x400';
+
+function PlayContainer() {
+  return <div data-testid="play-container">PlayContainer</div>;
+}
+
+export default function Thumbnail({ background = defaultBackground }) {
+  const [onHover, setOnHover] = useState(false);
+
+  return <div onMouseEnter={() => {
+    setOnHover(true);
+  }}
+    onMouseLeave={() => {
+      setOnHover(false);
+    }}
+  >
+    {
+      onHover && <PlayContainer />
+    }
     {background && <img data-testid="thumbnail-background" src={background} alt="" />
     }
   </div>;
