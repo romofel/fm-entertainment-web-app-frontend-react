28b4154540715c87615e1a721aa912218ce273b7
Felipe Romo <felipe@romofel.dev>
2022-08-13 01:32:41 -0500

    Set explicit size, round the border, hide the excess
    Expand image to the parent and change the fit to cover

--- src/components/Thumbnail/thumbnail.jsx
+++ src/components/Thumbnail/thumbnail.jsx
@@ -1,4 +1,7 @@
+/** @jsxImportSource @emotion/react */
+
 import { useState } from "react";
+import { css } from "@emotion/react";
 
 const defaultBackground = "https://placehold.co/600x400";
 
@@ -6,11 +9,25 @@ function PlayContainer() {
   return <div data-testid="play-container">PlayContainer</div>;
 }
 
+const thumbnailStyles = css`
+  width: 255px;
+  height: 174px;
+  border-radius: 8px;
+  overflow: hidden;
+
+  & img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
 export default function Thumbnail({ background = defaultBackground }) {
   const [onHover, setOnHover] = useState(false);
 
   return (
     <div
+      css={thumbnailStyles}
       onMouseEnter={() => {
         setOnHover(true);
       }}
